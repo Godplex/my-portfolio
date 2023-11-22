efcf440fc93dd10746be49ce9da9bfeb27a4e681
Julian Rincon <julian1918@hotmail.es>
2023-11-21 22:53:00 -0500

    first commit

--- src/common/StyledMenuItem.tsx
+++
@@ -1,7 +0,0 @@
-import styled from "@emotion/styled";
-import { Button, ButtonProps } from "@mui/material";
-
-export const StyledMenuItem = styled(Button)<ButtonProps>(() => ({
-    backgroundColor: 'transparent',
-    borderRadius:'0px!important',
-}));
